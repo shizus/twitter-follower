b70085adbf4147eeaf3e64294b36bbc5f4fb5390
Gabriel La Torre <latorregab@gmail.com>
2016-10-21 09:31:58 -0300

    Initiating repository

---
+++ twitter-follower.js
@@ -0,0 +1,10 @@
+var i = 0; 
+function seguir(botones, i) {
+	botones[i].click();
+	i++;
+	//var tiempo = Math.floor((Math.random() * 1000000) + 10000);
+	var tiempo = 5000;
+	setTimeout(seguir, tiempo, $(".user-actions-follow-button"), i);
+}
+setTimeout(seguir, 5000, $(".user-actions-follow-button"), i);
+
