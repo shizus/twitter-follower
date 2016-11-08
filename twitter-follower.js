adca9256427c5b39354422c4383a33452c09a93f
Gabriel La Torre <latorregab@gmail.com>
2016-11-08 15:07:55 -0300

    modifico el follower

--- twitter-follower.js
+++ twitter-follower.js
@@ -2,8 +2,8 @@ var i = 0;
 function seguir(botones, i) {
 	botones[i].click();
 	i++;
-	//var tiempo = Math.floor((Math.random() * 1000000) + 10000);
-	var tiempo = 5000;
+	var tiempo = Math.floor((Math.random() * 200000) + 10000);
+	//var tiempo = 5000;
 	setTimeout(seguir, tiempo, $(".user-actions-follow-button"), i);
 }
 setTimeout(seguir, 5000, $(".user-actions-follow-button"), i);
